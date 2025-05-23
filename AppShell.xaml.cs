428b7352e098c2b85e9b09eefe2ca84205cd685f
Sasha Manyonok <83176347+EriccCarmann@users.noreply.github.com>
2025-05-23 14:01:02 +0200

    changed project

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace HybridWebReactMaui;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace HybridWebReactMaui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
